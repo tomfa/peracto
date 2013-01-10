f4c34c68f2aa829211fc82d8efb2b93b10da086e
Tomas Fagerbekk <tomas.fagerbekk@ntnu.no>
2013-01-10 02:49:59 +0100

    Up and running. Code is kinda messy, but it works

--- peracto/peracto/settings.py
+++ peracto/peracto/settings.py
@@ -4,15 +4,15 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Tomas A. Fagerbekk', 'tomas@webutvikling.org'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'rbpidb',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -116,9 +116,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
+    'compleo',
 )
 
 # A sample logging configuration. The only tangible logging
--- peracto/peracto/urls.py
+++ peracto/peracto/urls.py
@@ -1,17 +1,22 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'peracto.views.home', name='home'),
-    # url(r'^peracto/', include('peracto.foo.urls')),
+    # url(r'^$', 'rbpi.views.home', name='home'),
+    # url(r'^rbpi/', include('rbpi.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^compleo/', include('compleo.urls')),
+    (r'^peracto/', include('compleo.urls')),
+    
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+
 )
+
+urlpatterns += staticfiles_urlpatterns()
